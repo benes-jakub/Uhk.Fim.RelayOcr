b45c2eaca678fc095e673ef0cda73029ca8bbd29
Jakub Beneš <jakub.benes@outlook.com>
2025-01-20 17:29:17 +0100

    run_find_best_crop script

--- src/run_find_best_crop.py
+++ src/run_find_best_crop.py
@@ -2,13 +2,13 @@ import sys
 from colorama import Fore, init
 from core.ocr import execute_ocr
 
-datasets = ["PE514024", "PE514F03", "PE014012"]
-experiments = ["A", "B", "C"]
-preprocessings = ["none", "binary", "mean", "gaus", "otsu", "dilate", "erode"]
+datasets = ["PE014012"]
+experiments = ["C"]
+preprocessings = ["none"]
 
 for ind, dataset in enumerate(datasets, start = 0): 
     for ind, preprocessing in enumerate(preprocessings, start = 0): 
         for ind, experiment in enumerate(experiments, start = 0):             
-            result = execute_ocr("../datasets/" + dataset + "/", dataset, experiment, 1, preprocessing, False, True)
+            result = execute_ocr("../datasets/" + dataset + "/", dataset, experiment, 0, preprocessing, True, True)
             print(dataset + " " + preprocessing + " " + experiment + ": " + result)            
         print("\n")
